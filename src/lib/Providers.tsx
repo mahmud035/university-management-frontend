3e7a21273bf3615e6ed75118d3ac27bfd4daf9cd
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2023-10-14 14:14:47 +0600

    49-1_ Initialize Our Project with Next.Js and Redux

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -6,9 +6,9 @@ import { Provider } from 'react-redux';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <>
       <Provider store={store}>{children}</Provider>
-    </div>
+    </>
   );
 };
 
