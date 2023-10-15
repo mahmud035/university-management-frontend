f471302c69f4296417ab315de51e118fd5168548
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2023-10-15 18:32:12 +0600

    49-8_Create Login page using reusable Form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,52 @@
+'use client';
+
+import Form from '@/components/Forms/Form';
+import FormInput from '@/components/Forms/FormInput';
+import { Button, Col, Row } from 'antd';
+import Image from 'next/image';
+import { SubmitHandler } from 'react-hook-form';
+import loginImage from '../../assets/login-image.svg';
+
+type FormValues = {
+  id: string;
+  password: string;
+};
+
 const LoginPage = () => {
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    try {
+      console.log(data);
+    } catch (error) {}
+  };
+
   return (
-    <div>
-      <h1>This is Login Page</h1>
-    </div>
+    <Row>
+      <Col sm={12} md={16} lg={16}>
+        <Image src={loginImage} width={500} alt="login-image" />
+      </Col>
+      <Col sm={12} md={8} lg={8}>
+        <h1 className="text-3xl font-semibold">Login Your Account</h1>
+        <div>
+          <Form submitHandler={onSubmit}>
+            <div>
+              <FormInput name="id" type="text" size="large" label="User Id" />
+            </div>
+            <div>
+              <FormInput
+                name="password"
+                type="password"
+                size="large"
+                label="User Password"
+              />
+            </div>
+
+            <Button type="primary" htmlType="submit" className="bg-blue-500">
+              Login
+            </Button>
+          </Form>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
